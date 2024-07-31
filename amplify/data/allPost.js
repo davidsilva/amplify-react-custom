128dc6649fb94adb86ad1fdd78552132aa85ed5d
David Silva <davidavilasilva@gmail.com>
2024-07-31 15:41:06 -0700

    Try explicitly setting ALL_ATTRIBUTES

--- amplify/data/allPost.js
+++ amplify/data/allPost.js
@@ -2,12 +2,10 @@ import * as ddb from "@aws-appsync/utils/dynamodb";
 
 export function request(ctx) {
   const { limit = 20, nextToken } = ctx.arguments;
-  console.log("allPost.js request", ctx);
-  return ddb.scan({ limit, nextToken });
+  return ddb.scan({ select: "ALL_ATTRIBUTES", limit, nextToken });
 }
 
 export function response(ctx) {
   const { items: posts = [], nextToken } = ctx.result;
-  console.log("allPost.js response", ctx);
   return { posts, nextToken };
 }
