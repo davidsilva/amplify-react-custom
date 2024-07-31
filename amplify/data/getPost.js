1bd99ea8877a099842e210a9729a0ba3a5b4f1c3
David Silva <davidavilasilva@gmail.com>
2024-07-31 13:16:45 -0700

    See if getPost logs

--- amplify/data/getPost.js
+++ amplify/data/getPost.js
@@ -1,7 +1,11 @@
 import * as ddb from "@aws-appsync/utils/dynamodb";
 
 export function request(ctx) {
+  console.log("getPost.js request", ctx);
   return ddb.get({ key: { id: ctx.args.id } });
 }
 
-export const response = (ctx) => ctx.result;
+export const response = (ctx) => {
+  console.log("getPost.js response", ctx);
+  return ctx.result;
+};
