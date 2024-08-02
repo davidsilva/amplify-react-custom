c75a76e9c213defebcfc21846f755ef943cc4edb
David Silva <davidavilasilva@gmail.com>
2024-08-02 11:36:12 -0700

    Set up Tailwind CSS and CSS for Amplify ui-react components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import "./index.css";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
