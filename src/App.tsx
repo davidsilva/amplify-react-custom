78ca4297e3721d0efdab41d74e474e8ae803f328
David Silva <davidavilasilva@gmail.com>
2024-08-02 11:38:18 -0700

    (1) Remove echo test; (2) add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import AddPost from "./components/AddPost";
 import ListPosts from "./components/ListPosts";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "../amplify/data/resource";
-import { useEffect } from "react";
-
-const client = generateClient<Schema>();
 
 function App() {
-  useEffect(() => {
-    const callEcho = async () => {
-      try {
-        await client.queries.echo({ content: "Hello, world!" });
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    callEcho();
-  }, []);
-
   return (
-    <main>
+    <main className="w-2/3 mx-auto">
       <ListPosts />
       <AddPost />
     </main>
