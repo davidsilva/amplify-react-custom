add13183ba2336dcf477c4e5bacd701074805cd5
David Silva <davidavilasilva@gmail.com>
2024-08-03 13:39:56 -0700

    Support restoring as well as archiving posts

--- amplify/data/batchArchivePosts.js
+++ amplify/data/batchArchivePosts.js
@@ -1,21 +1,26 @@
 import { util } from "@aws-appsync/utils";
 
 /* 
-* First we'll use BatchGetItem to get the posts for the provided ids argument.
-* Then we'll use BatchPutItem to set the isArchived attribute to true.
-* In the response, we'll return the updated posts as an items list. Ids of posts that could not be processed will be returned in the unprocessedKeys list.
+* This function takes input from batchGetPosts.js.
+* By default, the isArchived field of each post is set to true. A supplied "archive" argument can be used to set the field to false.
+* In the response, we return the updated posts as an items list. Ids of posts that could not be processed will be returned in the unprocessedKeys list.
 
 https://docs.amplify.aws/react/build-a-backend/data/connect-to-existing-data-sources/connect-external-ddb-table/#batchgetitem
+
+"Items that haven’t been processed are marked as null inside the data block and are placed inside the unprocessedItems block."
+https://docs.aws.amazon.com/appsync/latest/devguide/js-aws-appsync-resolver-reference-dynamodb-batch-put-item.html
 */
 
 export function request(ctx) {
   // We should get an "items" list of posts: prev.result.items
+  // Default archive to true. If false, we'll unarchive/restore the posts.
   console.log("batchArchivePosts request ctx", ctx);
+  const { archive = true } = ctx.args;
   const tableName = "PostTable";
   const postsToPut = ctx.prev.result.items.map((post) => {
     return util.dynamodb.toMapValues({
       ...post,
-      isArchived: true,
+      isArchived: archive,
     });
   });
 
